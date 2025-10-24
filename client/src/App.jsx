51910f32ac6795697be7d5d6a2963a995c9b478f
Omar Hossam <omar-hossam0@users.noreply.github.com>
2025-10-24 15:05:10 +0300

    Initial commit: Quick Store project

---
+++ client/src/App.jsx
@@ -0,0 +1,46 @@
+import { Toaster } from 'react-hot-toast'
+import { Route, Routes, useLocation } from "react-router-dom"
+import Footer from './components/Footer'
+import Navbar from "./components/Navbar"
+import Favorite from "./pages/Favorite"
+import Home from "./pages/Home"
+import MovieDetails from "./pages/MovieDetails"
+import Movies from "./pages/Movies"
+import MyBookings from "./pages/MyBookings"
+import SeatLayout from "./pages/SeatLayout"
+import ListShows from './pages/Admin/ListShows'
+import AdminSideBar from './components/admin/AdminSideBar'
+import AddShows from './pages/Admin/AddShows'
+import Dashboard from './pages/Admin/Dashboard'
+import Layout from './pages/Admin/Layout'
+import ListBookings from './pages/Admin/ListBookings'
+
+
+
+const App = () => {
+  const isAdminRoute = useLocation().pathname.startsWith('/admin')
+
+  return (
+    <>
+    <Toaster/>
+      {!isAdminRoute && <Navbar />}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/movies/:id" element={<MovieDetails />} />
+        <Route path="/movies/:id/:date" element={<SeatLayout />} />
+        <Route path="/my-booking" element={<MyBookings />} />
+        <Route path="/favorite" element={<Favorite />} />
+        <Route path='/admin/*' element={<Layout/>}>
+          <Route index element={<Dashboard />} />
+          <Route path='add-shows' element={<AddShows/>} />
+          <Route path='list-shows' element={<ListShows />} />
+          <Route path='list-bookings' element={<ListBookings />} />
+        </Route>
+      </Routes>
+      {!isAdminRoute && <Footer />}
+    </>
+  )
+}
+
+export default App

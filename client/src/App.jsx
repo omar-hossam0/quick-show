399073629eba818f7afa3b7e8e6734ae8b037b9d
omar hossam <amorhossam2005@gmail.com>
2026-05-24 17:12:07 +0300

    style: format code with consistent quotation marks and spacing in App, main, and Layout components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,26 @@
-import { Toaster } from 'react-hot-toast'
-import { Route, Routes, useLocation } from "react-router-dom"
-import Footer from './components/Footer'
-import Navbar from "./components/Navbar"
-import Favorite from "./pages/Favorite"
-import Home from "./pages/Home"
-import MovieDetails from "./pages/MovieDetails"
-import Movies from "./pages/Movies"
-import MyBookings from "./pages/MyBookings"
-import SeatLayout from "./pages/SeatLayout"
-import ListShows from './pages/Admin/ListShows'
-import AdminSideBar from './components/admin/AdminSidebar'
-import AddShows from './pages/Admin/AddShows'
-import Dashboard from './pages/Admin/Dashboard'
-import Layout from './pages/Admin/Layout'
-import ListBookings from './pages/Admin/ListBookings'
-
-
+import { Toaster } from "react-hot-toast";
+import { Route, Routes, useLocation } from "react-router-dom";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import Favorite from "./pages/Favorite";
+import Home from "./pages/Home";
+import MovieDetails from "./pages/MovieDetails";
+import Movies from "./pages/Movies";
+import MyBookings from "./pages/MyBookings";
+import SeatLayout from "./pages/SeatLayout";
+import ListShows from "./pages/Admin/ListShows";
+import AdminSideBar from "./components/admin/AdminSidebar";
+import AddShows from "./pages/Admin/AddShows";
+import Dashboard from "./pages/Admin/Dashboard";
+import Layout from "./pages/Admin/Layout";
+import ListBookings from "./pages/Admin/ListBookings";
 
 const App = () => {
-  const isAdminRoute = useLocation().pathname.startsWith('/admin')
+  const isAdminRoute = useLocation().pathname.startsWith("/admin");
 
   return (
     <>
-    <Toaster/>
+      <Toaster />
       {!isAdminRoute && <Navbar />}
       <Routes>
         <Route path="/" element={<Home />} />
@@ -31,16 +29,16 @@ const App = () => {
         <Route path="/movies/:id/:date" element={<SeatLayout />} />
         <Route path="/my-booking" element={<MyBookings />} />
         <Route path="/favorite" element={<Favorite />} />
-        <Route path='/admin/*' element={<Layout/>}>
+        <Route path="/admin/*" element={<Layout />}>
           <Route index element={<Dashboard />} />
-          <Route path='add-shows' element={<AddShows/>} />
-          <Route path='list-shows' element={<ListShows />} />
-          <Route path='list-bookings' element={<ListBookings />} />
+          <Route path="add-shows" element={<AddShows />} />
+          <Route path="list-shows" element={<ListShows />} />
+          <Route path="list-bookings" element={<ListBookings />} />
         </Route>
       </Routes>
       {!isAdminRoute && <Footer />}
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
-import { ClerkProvider } from '@clerk/clerk-react'
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import { ClerkProvider } from "@clerk/clerk-react";
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
 const RootApp = (
   <StrictMode>
@@ -12,9 +12,9 @@ const RootApp = (
       <App />
     </BrowserRouter>
   </StrictMode>
-)
+);
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   PUBLISHABLE_KEY ? (
     <StrictMode>
       <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
@@ -25,5 +25,5 @@ createRoot(document.getElementById('root')).render(
     </StrictMode>
   ) : (
     RootApp
-  )
-)
+  ),
+);
